f3ede703d07a86b15218297d2fb34ecbd28b1a34
Kelvin Winsters <kelvinwinsters@gmail.com>
2025-06-20 02:27:30 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,16 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+
+    # Repeat the process for each element in the list
+    for i in range(n):
+        # Go through the list up to the last unsorted element
+        for j in range(0, n - 1 - i):
+            # If the current item is bigger than the next one, swap them
+            if arr[j] > arr[j + 1]:
+                # Swap using a temp variable
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+
+    return arr
+
