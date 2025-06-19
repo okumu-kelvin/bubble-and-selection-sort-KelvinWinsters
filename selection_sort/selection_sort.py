9a1fc68a88f33f4ff7e184a4449c4040a8a99896
Kelvin Winsters <kelvinwinsters@gmail.com>
2025-06-20 02:27:57 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,17 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i  # Start by assuming the current index has the smallest value
+
+        # Find the index of the smallest value in the remaining list
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap the values using a temporary variable (temp)
+        temp = arr[i]
+        arr[i] = arr[min_index]
+        arr[min_index] = temp
+
+    return arr
